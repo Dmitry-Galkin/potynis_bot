5f8f0da6240cb09d6d44397abd330a0c3130b988
Dmitrii Galkin <d.galkin.89@gmail.com>
2026-02-23 11:24:52 +0300

    add get_chat_id command

--- app/bot/handlers/admin.py
+++ app/bot/handlers/admin.py
@@ -1,3 +1,5 @@
+from contextlib import suppress
+
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
@@ -12,3 +14,15 @@ async def process_start_command(message: Message):
     await message.answer(
         text="Выберите, что вы хотите сделать", reply_markup=main_admin_keyboard()
     )
+
+
+@admin_router.message(Command(commands="get_chat_id"))
+async def process_get_chat_id_command(message: Message, **kwargs):
+    """Узнать id группы."""
+    print(f"---> chat_id: {message.chat.id}")
+    await delete_message(kwargs["bot"], message.chat.id, message.message_id)
+
+
+async def delete_message(bot, chat_id: int, message_id: int):
+    with suppress(Exception):
+        await bot.delete_message(chat_id=chat_id, message_id=message_id)
